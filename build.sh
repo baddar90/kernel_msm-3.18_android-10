c58e1bdf9a40f61ae2c2f904d9f7f4e1d08ebc0e
baddar <mnbaddar1990@gmail.com>
2020-01-03 09:57:28 +0000

    add build script

---
+++ build.sh
@@ -0,0 +1,11 @@
+export KBUILD_BUILD_USER=baddar
+export KBUILD_BUILD_HOST=linux
+make O=$HOME/out ARCH=arm64 lineageos_axon7_defconfig
+
+PATH="/home/mohammadbaddar1990/dragon/bin:/home/mohammadbaddar1990/gcc-64/bin:/home/mohammadbaddar1990/gcc-32/bin:${PATH}" \
+make -j$(nproc --all) O=$HOME/out \
+                      ARCH=arm64 \
+                      CC=clang \
+                      CLANG_TRIPLE=aarch64-linux-gnu- \
+                      CROSS_COMPILE=aarch64-linux-android- \
+                      CROSS_COMPILE_ARM32=arm-linux-androideabi-
